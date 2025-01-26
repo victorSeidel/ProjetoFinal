b3bd6951594a9b0a6ec5182a17d070d11bad881b
Victor Seidel <victorgabrielms88@gmail.com>
2025-01-26 10:54:25 -0300

    Adicionando a classe JogoDeTabuleiro (V1)

---
+++ include/JogoDeTabuleiro.hpp
@@ -0,0 +1,30 @@
+#ifndef JOGODETABULEIRO_HPP
+#define JOGODETABULEIRO_HPP
+
+#include <vector>
+#include <iostream>
+
+class JogoDeTabuleiro 
+{
+    protected:
+    std::vector<std::vector<char>> tabuleiro;
+    int linhas;
+    int colunas;
+
+    public:
+    JogoDeTabuleiro(int linhas, int colunas);
+
+    virtual ~JogoDeTabuleiro();
+
+    virtual void ImprimirTabuleiro() const;
+
+    virtual bool JogadaValida(int linha, int coluna) const;
+
+    virtual void RealizarJogada(int linha, int coluna, char jogador);
+
+    virtual void Reiniciar();
+
+    virtual bool VerificarVitoria(char jogador) const = 0;
+};
+
+#endif
