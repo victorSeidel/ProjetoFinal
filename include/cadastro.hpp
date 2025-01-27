f97ccd9b5f1b437f8d6ce57878c8f5abd9554742
93igortvrs93 <igortavsilva05@gmail.com>
2025-01-27 19:46:21 -0300

    atualizando os arquivos de cabeçalho
    agora a implementação está nos arquivos ".cpp"

--- include/cadastro.hpp
+++ include/cadastro.hpp
@@ -9,37 +9,10 @@ class cadastro{
     private:
     map<string,jogador> jogadores; //fiz assim para que possamos ordenar os jogadores em ordem alfabética, aí cada um deles pode ter como chave o seu apelido, que é único
     public:
-    void add_jogador(jogador novo){
-        this->jogadores.insert({novo.get_apld(),novo});
-    }
-    void rmv_jogador(jogador removido){
-        this->jogadores.erase(removido.get_apld());
-    }
-    void listar_jgdrs(){
-        map<string,jogador>::iterator it;
-        for(it = this->jogadores.begin();it != this->jogadores.end();it++){
-            it->second.imprime_estatisticas();
-        }
-    }
-    void export_jgdrs(){
-        fstream outpt;
-        outpt.open("jogadores_salvos.txt", ios::app);
-        map<string,jogador>::iterator it;
-        for(it = this->jogadores.begin();it != this->jogadores.end();it++){
-            outpt << it->second.export_estatisticas();
-        }
-        outpt.close();
-    }
-    void import_jgdrs(){
-        fstream inpt;
-        inpt.open("jogadores_salvos.txt", ios::in);
-        while(!(inpt.eof())){
-            string nm, apld;
-            int vv,dv,vr,dr,vl,dl;
-            inpt >> apld >> nm >> vv >> dv >> vr >> dr >> vl >> dl;
-            jogador novo(nm,apld,vv,dv,vr,dr,vl,dl);
-            this->add_jogador(novo);
-        }
-    }
+    void add_jogador(jogador);
+    void rmv_jogador(jogador);
+    void listar_jgdrs();
+    void export_jgdrs();
+    void import_jgdrs();
 };
 #endif
--- include/jogador.hpp
+++ include/jogador.hpp
@@ -14,60 +14,13 @@ class jogador{
     string nome;
     string apelido;
     public:
-    jogador(string nm,string apld){ //para criar um jogador completamente novo
-        this->nome = nm;
-        this->apelido = apld;
-        this->vit_velha = 0;
-        this->derrt_velha = 0;
-        this->vit_rvrsi = 0;
-        this->derrt_rvrsi = 0;
-        this->vit_lig = 0;
-        this->derrt_lig = 0;
-    }
-    jogador(string nm,string apld,int vv,int dv,int vr,int dr,int vl,int dl){ //para criar um jogador a partir de um cadastro já salvo
-        this->nome = nm;
-        this->apelido = apld;
-        this->vit_velha = vv;
-        this->derrt_velha = dv;
-        this->vit_rvrsi =vr;
-        this->derrt_rvrsi = dr;
-        this->vit_lig = vl;
-        this->derrt_lig = dl;
-    }
-    void imprime_estatisticas(){
-        cout << this->apelido << " " << this->nome << endl;
-        cout << "REVERSI - V: " << this->vit_rvrsi << "D: " << this-> derrt_rvrsi << endl;
-        cout << "LIG 4 - V: " << this->vit_lig << "D: " << this-> derrt_lig << endl;
-        cout << "VELHA - V: " << this->vit_velha << "D: " << this-> derrt_velha << endl;
-    }
-    void add_vit(string jogo){ //para quem for usar: lembrar de chamar a função com as palavras certinhas, ou não vai contar a vitória ou derrota, e teremos um erro
-        if(jogo == "velha"){
-            this->vit_velha++;
-        }else if(jogo == "reversi"){
-            this->vit_rvrsi++;
-        }else if(jogo == "lig"){
-            this->vit_lig++;
-        }
-    }
-    void add_derrt(string jogo){
-        if(jogo == "velha"){
-            this->derrt_velha++;
-        }else if(jogo == "reversi"){
-            this->derrt_rvrsi++;
-        }else if(jogo == "lig"){
-            this->derrt_lig++;
-        }
-    }
-    string get_nome(){
-        return this->nome;
-    }
-    string get_apld(){
-        return this->apelido;
-    }
-    string export_estatisticas(){
-        string expt;
-        expt = this->apelido + " " + this->nome + " " + (char) this->vit_velha + " " + (char) this->derrt_velha + " " + (char) this->vit_rvrsi + " " + (char)this->derrt_rvrsi + " " + (char)this->vit_lig + " " + (char)this->derrt_lig + '\n';
-        return expt;
-    }
+    jogador(string,string); //para criar um jogador completamente novo
+    jogador(string,string,int,int,int,int,int,int);//para criar um jogador a partir de um cadastro já salvo
+    void imprime_estatisticas();
+    void add_vit(string jogo); //para quem for usar: lembrar de chamar a função com as palavras certinhas, ou não vai contar a vitória ou derrota, e teremos um erro
+    void add_derrt(string jogo);
+    string get_nome();
+    string get_apld();
+    string export_estatisticas();
 };
 #endif
