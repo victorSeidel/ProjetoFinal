db012ee83195cd02912ce095d54e290361c245d5
Victor Seidel <victorgabrielms88@gmail.com>
2025-01-28 20:23:45 -0300

    Update Cadastro.hpp

--- include/Cadastro.hpp
+++ include/Cadastro.hpp
@@ -6,9 +6,8 @@
 #include "jogador.hpp"
 using namespace std;
 class cadastro{
-    private:
-    map<string,jogador> jogadores; //fiz assim para que possamos ordenar os jogadores em ordem alfabética, aí cada um deles pode ter como chave o seu apelido, que é único
     public:
+    map<string,jogador> jogadores; //fiz assim para que possamos ordenar os jogadores em ordem alfabética, aí cada um deles pode ter como chave o seu apelido, que é único
     void cadastrar_jogador();
     void add_jogador(jogador);
     void rmv_jogador(jogador);
