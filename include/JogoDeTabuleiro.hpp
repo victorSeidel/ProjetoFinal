ee68f1e44d2cac5e1ab4decad82dc56fdd60472e
Victor Seidel <victorgabrielms88@gmail.com>
2025-01-28 09:34:23 -0300

    Formatando JogoDeTabuleiro.hpp

--- include/JogoDeTabuleiro.hpp
+++ include/JogoDeTabuleiro.hpp
@@ -4,26 +4,26 @@
 #include <vector>
 #include <iostream>
 
-class JogoDeTabuleiro {
-protected:
-    std::vector<std::vector<char>> tabuleiro;
-    int linhas;
-    int colunas;
-
-public:
-    // Construtor: inicializa apenas tamanho e estrutura do tabuleiro.
-    JogoDeTabuleiro(int linhas, int colunas) 
-        : linhas(linhas), colunas(colunas), tabuleiro(linhas, std::vector<char>(colunas, ' ')) {}
-
-    virtual ~JogoDeTabuleiro() = default;
-
-    // Métodos puramente virtuais (obrigatórios para classes derivadas):
-    virtual void ImprimirTabuleiro() const = 0;
-    virtual bool JogadaValida(int linha, int coluna) const = 0;
-    virtual void RealizarJogada(int linha, int coluna, char jogador) = 0;
-    virtual void Reiniciar() = 0;
-    virtual bool VerificarVitoria(char jogador) const = 0;
-    virtual void ExecutarPartida() = 0;
+class JogoDeTabuleiro 
+{
+    protected:
+        std::vector<std::vector<char>> tabuleiro;
+        int linhas;
+        int colunas;
+    
+    public:
+        // Construtor: inicializa apenas tamanho e estrutura do tabuleiro.
+        JogoDeTabuleiro(int linhas, int colunas) : linhas(linhas), colunas(colunas), tabuleiro(linhas, std::vector<char>(colunas, '.')) {}
+    
+        virtual ~JogoDeTabuleiro() = default;
+    
+        // Métodos puramente virtuais (obrigatórios para classes derivadas):
+        virtual void ImprimirTabuleiro() const = 0;
+        virtual bool JogadaValida(int linha, int coluna) const = 0;
+        virtual void RealizarJogada(int linha, int coluna, char jogador) = 0;
+        virtual void Reiniciar() = 0;
+        virtual bool VerificarVitoria(char jogador) const = 0;
+        virtual void ExecutarPartida() = 0;
 };
 
 #endif
