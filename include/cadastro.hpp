3d28edb725d1737103051b9957a3dd718a2199f6
93igortvrs93 <igortavsilva05@gmail.com>
2025-01-28 19:31:21 -0300

    mudando o acesso do atributo

---
+++ include/cadastro.hpp
@@ -0,0 +1,17 @@
+#ifndef CADASTRO_H
+#define CADASTRO_H
+#include <string>
+#include <map>
+#include <fstream>
+#include "jogador.hpp"
+using namespace std;
+class cadastro{
+    public:
+    map<string,jogador> jogadores; //fiz assim para que possamos ordenar os jogadores em ordem alfabética, aí cada um deles pode ter como chave o seu apelido, que é único
+    void add_jogador(jogador);
+    void rmv_jogador(jogador);
+    void listar_jgdrs();
+    void export_jgdrs();
+    void import_jgdrs();
+};
+#endif
