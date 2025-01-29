e27acb235ff6e3f0299c72f72b157e4cea2de92c
Victor Seidel <victorgabrielms88@gmail.com>
2025-01-28 22:28:07 -0300

    Update JogoDaVelha.hpp

--- include/JogoDaVelha.hpp
+++ include/JogoDaVelha.hpp
@@ -7,12 +7,12 @@ class JogoDaVelha: public JogoDeTabuleiro{
     public:
     JogoDaVelha();
     virtual ~JogoDaVelha();
-    bool JogadaValida(int linha, int coluna) override;
+    bool JogadaValida(int linha, int coluna) const override;
     void Reiniciar() override;
-    bool VerificarVitoria(char)const override;
+    bool VerificarVitoria(char) const override;
     void RealizarJogada(int, int , char) override;
     int ExecutarPartida() override;
     int Jogar() override;
-    void ImprimirTabuleiro() override;
+    void ImprimirTabuleiro() const override;
 };
 #endif
