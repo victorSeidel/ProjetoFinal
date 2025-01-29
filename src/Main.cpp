e0ad715625e1eb24311e93af6dc88d29ed931978
93igortvrs93 <igortavsilva05@gmail.com>
2025-01-29 00:31:51 -0300

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,9 @@
 #include "ExecutarPartida.hpp"
 
 #include <iostream>
-
+/**
+* @brief Mostra as possíveis opções de entrada para cada funcionalidade do sistema
+*/
 void Mostrar()
 {
     std::cout << "Escolha o que deseja fazer: \n";
@@ -13,9 +15,9 @@ int main()
 {
     std::cout << "Ola, seja muito bem-vindo(a)! \n";
     Mostrar();
-    int opcao=0;
+    int opcao=0; /** @brief Armazena a opção escolhida pelo jogador. */
         std::cin >> opcao;
-                ExecutarPartida jogo; 
+                ExecutarPartida jogo; /** @brief Jogo a ser jogado */
                 jogo.IniciarJogo();
 
     return 0;
