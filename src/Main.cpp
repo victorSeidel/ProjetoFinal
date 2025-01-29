5547e14259d204360bec00b188ffa610593c78dc
Mateus Vieira <hmateus2501@gmail.com>
2025-01-29 00:22:39 -0300

    update

--- src/Main.cpp
+++ src/Main.cpp
@@ -12,38 +12,11 @@ void Mostrar()
 int main() 
 {
     std::cout << "Olá, seja muito bem-vindo(a)! \n";
-
-    do
-    {
-        Mostrar();
-    
-        int opcao = 0;
+    Mostrar();
+    int opcao=0;
         std::cin >> opcao;
-    
-        switch(opcao)
-        {
-            case 1:
                 ExecutarPartida jogo; 
                 jogo.IniciarJogo();
-    
-                break;
-            
-            case 2:
-                std::cout << "----- Cadastrar Novo Jogador ----- \n";
-                break;
-            
-            case 3:
-                std::cout << "----- Lista de Jogadores ----- \n";
-                break;
-
-            case 4:
-                break;
-            
-            default:
-                std::cout << "Erro: Opção inválida! \n";
-                break;
-        }
-    } while (opcao != 4);
 
     return 0;
 }
