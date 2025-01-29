e495668cc1734b083183dbe06e1df8b2faa96f54
Victor Seidel <victorgabrielms88@gmail.com>
2025-01-28 22:52:43 -0300

    Organizando a Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,51 +4,69 @@
 
 #include <iostream>
 
-int main() 
+void Mostrar()
 {
-    std::cout << "Olá, seja muito bem-vindo(a)! \n";
     std::cout << "Escolha o que deseja fazer: \n";
     std::cout << "1. Jogar \n";
     std::cout << "2. Cadastrar Jogador \n";
     std::cout << "3. Listar Jogadores \n";
+    std::cout << "4. Encerrar Programa \n";
+}
 
-    int opcao;
-    std::cin >> opcao;
+int main() 
+{
+    std::cout << "Olá, seja muito bem-vindo(a)! \n";
 
-    cadastro cadastro;
-
-    string ap1, ap2;
-    jogador jogador1, jogador2;
-
-    switch(opcao)
+    int l = 1;
+    while(l)
     {
-        case 1:
-            std::cout << "Digite o apelido do jogador 1: ";
-            std::cin >> ap1;
-
-            std::cout << "Digite o apelido do jogador 2: ";
-            std::cin >> ap2;
-
-            ExecutarPartida jogo; 
-            jogo.IniciarJogo(jogador1, jogador2);
-
-            break;
-        case 2:
-            std::cout << "----- Cadastrar Novo Jogador ----- \n";
-
-            cadastro.cadastrar_jogador();
-            cadastro.export_jgdrs();
+        Mostrar();
+    
+        int opcao;
+        std::cin >> opcao;
+    
+        cadastro cadastro;
+    
+        string ap1, ap2;
+        jogador jogador1, jogador2;
+    
+        switch(opcao)
+        {
+            case 1:
+                std::cout << "Digite o apelido do jogador 1: ";
+                std::cin >> ap1;
+    
+                std::cout << "Digite o apelido do jogador 2: ";
+                std::cin >> ap2;
+    
+                ExecutarPartida jogo; 
+                jogo.IniciarJogo(jogador1, jogador2);
+    
+                break;
             
-            break;
-        case 3:
-            std::cout << "----- Lista de Jogadores ----- \n";
+            case 2:
+                std::cout << "----- Cadastrar Novo Jogador ----- \n";
+    
+                cadastro.cadastrar_jogador();
+                cadastro.export_jgdrs();
+                
+                break;
+            
+            case 3:
+                std::cout << "----- Lista de Jogadores ----- \n";
+    
+                cadastro.listar_jgdrs();
+    
+                break;
 
-            cadastro.listar_jgdrs();
-
-            break;
-        default:
-            std::cout << "Erro: Opção inválida! \n";
-            break;
+            case 4:
+                l = 0;
+                break;
+            
+            default:
+                std::cout << "Erro: Opção inválida! \n";
+                break;
+        }
     }
 
     return 0;
