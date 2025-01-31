d015c67865de27e721eb9d800b5b423fac8fa377
93igortvrs93 <igortavsilva05@gmail.com>
2025-01-30 21:25:40 -0300

    comentários para documentação em cadastro.hpp

--- include/cadastro.hpp
+++ include/cadastro.hpp
@@ -6,14 +6,25 @@
 #include <string>
 #include <vector>
 #include "jogador.hpp"
-
+/**
+* @brief Classe que cuida do cadastro de jogadores. 
+*/
 class CadastroJogadores {
 private:
+/**
+* @brief Mapa com cada jogador cadastrado, utilizando o apelido do jogador como chave.
+*/
     std::map<std::string, Jogador> jogadores;
+/**
+* @brief Nome do arquivo em que são salvos os dados dos jogadores.
+*/
     const std::string arquivo = "jogadores.txt";
 
 public:
     // Operações básicas
+/**
+* @brief Método para adicionar jogadores ao mapa.
+*/
     std::string cadastrar(const std::string& apelido, const std::string& nome);
     std::string remover(const std::string& apelido);
     void listar(char ordenarPor);
