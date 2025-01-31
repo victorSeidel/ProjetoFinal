9fecea0d2729d565271bdd9ebe031a3daac3a14c
93igortvrs93 <igortavsilva05@gmail.com>
2025-01-30 23:10:58 -0300

    comentários p/ documentacao  JogoDaVelha.hpp

--- include/JogoDaVelha.hpp
+++ include/JogoDaVelha.hpp
@@ -2,20 +2,31 @@
 #define JOGO_DA_VELHA_H
 
 #include "JogoDeTabuleiro.hpp"
-
+/**
+* @brief Classe que implementa o jogo da velha, especializando as funções da classe abstrata.
+*/
 class JogoDaVelha: public JogoDeTabuleiro{
 
 private:
 
 
-    std::string jogadorX;
-    std::string jogadorO;
+    std::string jogadorX; /** Representa o primeiro jogador */
+    std::string jogadorO; /** Representa o segundo jogador. */
+/**
+* @brief Obtém o apelido de um jogador.
+* @param simbolo "X" ou "O" para representar cada jogador.
+* @return O apelido em si.
+*/
     std::string ObterApelido(char simbolo) const;
 
 
 public:
 
-
+/**
+* @brief Assinala "X" e "O" para os respectivos jogadores.
+* @param x O apelido do jogador que terá "X" como símbolo.
+* @param o O apelido do jogador que terá "O" como símbolo.
+*/
     void DefinirApelidos(const std::string& x, const std::string& o);
     JogoDaVelha();
     virtual ~JogoDaVelha() = default;
