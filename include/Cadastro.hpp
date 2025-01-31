0ca2d955911d5b8998d21f106c727ea7d5a5abde
Victor Seidel <victorgabrielms88@gmail.com>
2025-01-30 22:07:59 -0300

    Comentando/Documentando Cadastro.hpp

--- include/Cadastro.hpp
+++ include/Cadastro.hpp
@@ -9,33 +9,58 @@
 /**
 * @brief Classe que cuida do cadastro de jogadores. 
 */
-class CadastroJogadores {
-private:
-/**
-* @brief Estrutura de dados Mapp para armazenar os jogadores cadastrados, utilizando o apelido do jogador como chave.
-*/
-    std::map<std::string, Jogador> jogadores;
-/**
-* @brief Nome do arquivo em que são salvos os dados dos jogadores.
-*/
-    const std::string arquivo = "jogadores.txt";
+class CadastroJogadores 
+{
+    private:
+        /**
+        * @brief Estrutura de dados Mapp para armazenar os jogadores cadastrados, utilizando o apelido do jogador como chave.
+        */
+        std::map<std::string, Jogador> jogadores;
+        /**
+        * @brief Nome do arquivo em que são salvos os dados dos jogadores.
+        */
+        const std::string arquivo = "jogadores.txt";
 
-public:
-    // Operações básicas
-/**
-* @brief Método para adicionar jogadores ao mapa.
-*/
-    std::string cadastrar(const std::string& apelido, const std::string& nome);
-    std::string remover(const std::string& apelido);
-    void listar(char ordenarPor);
-
-    // Persistência
-    void salvarEmArquivo();
-    void carregarDoArquivo();
-
-    // Consultas
-    bool jogadorExiste(const std::string& apelido) const;
-    Jogador& getJogador(const std::string& apelido);
+    public:
+        // Operações básicas
+        /**
+        * @brief Método para cadastrar novos jogadores e adicionar ao mapa.
+        * @param apelido apelido único para cadastro
+        * @param nome nome do jogador
+        */
+        std::string cadastrar(const std::string& apelido, const std::string& nome);
+        /**
+        * @brief Método para remover cadastro de jogadores.
+        * @param apelido remover cadastro com tal apelido
+        */
+        std::string remover(const std::string& apelido);
+        /**
+        * @brief Método para listar os jogadores cadastrados.
+        * @param ordernarPor ordernar por nome ou apelido
+        */
+        void listar(char ordenarPor);
+    
+        // Persistência
+        /**
+        * @brief Salvar os dados dos jogadores cadastrados em um arquivo de texto.
+        */
+        void salvarEmArquivo();
+        /**
+        * @brief Carregar os dados de jogadores salvos em um arquivo de texto.
+        */
+        void carregarDoArquivo();
+    
+        // Consultas
+        /**
+        * @brief Verifica se existe um cadastro.
+        * @param apelido apelido que será verificado
+        */
+        bool jogadorExiste(const std::string& apelido) const;
+        /**
+        * @brief Retorna um objeto do tipo jogador.
+        * @param apelido retorna o jogador com esse apelido
+        */
+        Jogador& getJogador(const std::string& apelido);
 };
 
 #endif
